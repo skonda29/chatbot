12013ba06a25fe0979f3ba2e676afdedfa850ac1
Srinitya Kondapally <60264570+skonda29@users.noreply.github.com>
2025-04-24 11:25:40 -0700

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ app = FastAPI()
 # Allow CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://reachoutforhelp.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
